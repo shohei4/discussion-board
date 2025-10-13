71f373abeed76f929d82cd24cc7ffe8e632354f6
DESKTOP-4F200RJ\user <qoo728qoo@gmail.com>
2025-10-13 15:16:04 +0900

    Initial commit

---
+++ discussion-board/src/test/java/com/example/discussion_board/DiscussionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discussion_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
