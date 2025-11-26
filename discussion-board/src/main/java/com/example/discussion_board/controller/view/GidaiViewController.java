8d3e8856c848a2b272d47e7f839673558d736a91
DESKTOP-NSSQOB1\shohe <qoo728qoo@gmail.com>
2025-11-26 16:46:26 +0900

    議題登録画面を変更

--- discussion-board/src/main/java/com/example/discussion_board/controller/view/GidaiViewController.java
+++ discussion-board/src/main/java/com/example/discussion_board/controller/view/GidaiViewController.java
@@ -1,8 +1,11 @@
 package com.example.discussion_board.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.discussion_board.model.Genre;
 
 @Controller
 @RequestMapping("/view/gidai")
@@ -14,7 +17,8 @@ public class GidaiViewController {
 	}
 	
 	@GetMapping("registration")
-	public String viewRegistration() {
+	public String viewRegistration(Model model) {
+		model.addAttribute("genres", Genre.values());
 		return "gidai/registration";
 	}
 
