ae6310421f9d09d03ae8b49c9b21479a8c2fedbd
DESKTOP-NSSQOB1\shohe <qoo728qoo@gmail.com>
2026-01-15 18:27:08 +0900

    ・navbarのフロント作成 ・検索分岐のrepositoryメソッドをJPQLで作成

--- discussion-board/src/main/java/com/example/discussion_board/repository/GidaiRepository.java
+++ discussion-board/src/main/java/com/example/discussion_board/repository/GidaiRepository.java
@@ -3,6 +3,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.discussion_board.entity.Gidai;
@@ -11,6 +13,15 @@ import com.example.discussion_board.model.Genre;
 
 @Repository
 public interface GidaiRepository extends JpaRepository<Gidai, Long> {
+	@Query("SELECT g FROM Gidai g WHERE " +
+	           "g.isDeleted = false AND " + // 削除されていないものに限定
+	           "(:genre IS NULL OR g.genre = :genre) AND " +
+	           "(:keyword IS NULL OR g.title LIKE %:keyword%)")
+	    List<Gidai> searchGidai(
+	        @Param("genre") Genre genre, 
+	        @Param("keyword") String keyword
+	    );
+	
 	List<Gidai> findAllByIsDeletedFalse();
 	Optional<Gidai> findByIdAndIsDeletedFalse(Long Id);
 	List<Gidai> findByGenreAndIsDeletedFalse(Genre Genre);
