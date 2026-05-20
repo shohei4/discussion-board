8fb348d6bfb5e94f12e83641d1f61d6e5cf14466
DESKTOP-NSSQOB1\shohe <qoo728qoo@gmail.com>
2026-05-20 15:58:47 +0900

    ・JSのutilメソッドとしてDateFormatterを作成 ・議論コメントの投稿日のフォーマットを調整

--- discussion-board/src/main/resources/static/js/utils.js
+++ discussion-board/src/main/resources/static/js/utils.js
@@ -23,4 +23,15 @@ function showFieldErrors(errors) {
 		if (input) input.classList.add("is-invalid");
 		if (feedback) feedback.innerText = errors[field];
 	}
+	
+	// 日時フォーマット統一関数
+function formatDateTime(dateStr) {
+    const date = new Date(dateStr);
+    const yyyy = date.getFullYear();
+    const MM = String(date.getMonth() + 1).padStart(2, '0');
+    const dd = String(date.getDate()).padStart(2, '0');
+    const hh = String(date.getHours()).padStart(2, '0');
+    const mm = String(date.getMinutes()).padStart(2, '0');
+    return `${yyyy}/${MM}/${dd} ${hh}:${mm}`;
+}
 }
